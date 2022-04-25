68faad4428a53ecfb9540eac093a73dd422a8b16
Ciro Mota <ciro.mota@gmail.com>
2022-04-25 17:05:59 -0300

    fix: Incorrect download folder, missing permissions to extract, missing application icon path and visual improvements

--- Firefox-DeSnap.sh
+++ Firefox-DeSnap.sh
@@ -8,7 +8,7 @@
 ## LICENSE:
 ###		  GPLv3. <https://github.com/ciro-mota/firefox-desnap/blob/main/LICENSE>
 ## CHANGELOG:
-### 		Last Update: 24/04/2022. <https://github.com/ciro-mota/firefox-desnap/commits/main>
+### 		Last Update: 25/04/2022. <https://github.com/ciro-mota/firefox-desnap/commits/main>
 
 ### Download Links.
 url_firefox_br="https://ftp.mozilla.org/pub/firefox/releases/99.0.1/linux-x86_64/pt-BR/firefox-99.0.1.tar.bz2"
@@ -17,37 +17,44 @@ url_firefox_en="https://ftp.mozilla.org/pub/firefox/releases/99.0.1/linux-x86_64
 # ---------------------------------------------------------------------------------------------------- #
 # ------------------------------------------ TEST AND EXECUTION -------------------------------------- #
 ### Check GNOME or Plasma.
-if [[ $(gnome-shell --version | awk '{print $1}') = "GNOME" ]];
+if [[ $(gnome-shell --version | awk '{print $1}') = "GNOME" ]] >/dev/null 2>&1;
 then        
       echo ""
-	    echo -e "\e[32;1mGNOME Detected. Continuing script...\e[m"
-	    echo ""
-elif [[ $(plasmashell --version | awk '{print $1}') = "plasmashell" ]];
+	echo -e "\e[32;1mGNOME Detected. Continuing script...\e[m"
+	echo ""
+elif [[ $(plasmashell --version | awk '{print $1}') = "plasmashell" ]] >/dev/null 2>&1;
 then
       echo ""
-	    echo -e "\e[32;1mPlasma Detected. Continuing script...\e[m"
-	    echo ""
+	echo -e "\e[32;1mPlasma Detected. Continuing script...\e[m"
+	echo ""
 else
       echo ""
       echo -e "\e[31;1mDE not supported by this script.\e[m"
-	    exit 1
+	exit 1
 fi	
 
 # ------------------------------------------------------------------------------------------------------ #
 # --------------------------------------------- CHOOSE LANGUAGE ---------------------------------------- #
 ### Choose version to download.
-echo -e "Type BR to download Brazilian Portuguese version or type EN for English version:"
+echo -e "Type \e[33;1mBR\e[m to download Brazilian Portuguese version or type \e[33;1mEN\e[m for English version:\n"
 read -r download
 
 case $download in
     br|BR)
-    	wget -cq --show-progress "$url_firefox_br" -P /home/teste/"$(id -nu 1000)"
+      echo ""
+      echo -e "\e[32;1mDownloading...\e[m"
+      echo ""
+    	wget -cq --show-progress "$url_firefox_br" -P /home/"$(id -nu 1000)"
 		;;
     en|EN)
-    	wget -cq --show-progress "$url_firefox_en" -P /home/teste/"$(id -nu 1000)"
+      echo ""
+      echo -e "\e[32;1mDownloading...\e[m"
+      echo ""
+    	wget -cq --show-progress "$url_firefox_en" -P /home/"$(id -nu 1000)"
     ;;  
     *)
-      echo "Incorrect option. Leaving..."
+      echo ""
+      echo -e "\e[31;1mIncorrect option. Leaving...\e[m"
       exit 1
 		;;
 esac
@@ -62,9 +69,10 @@ Pin-Priority: -1
 EOF
 
 ### Instalation of Firefox Release.
-echo -e "\e[32;1mDownloading...\e[m"
 echo ""
-tar xjf /home/teste/"$(id -nu 1000)"/firefox*.bz2 -C /opt
+echo -e "\e[32;1mInstaling...\e[m"
+echo ""
+sudo tar xjf /home/"$(id -nu 1000)"/firefox*.bz2 -C /opt
 sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
 sudo chown -R "$(whoami)":"$(whoami)" /opt/firefox*
 
@@ -75,7 +83,7 @@ Name=Firefox
 GenericName=Web Browser
 Comment=Browse the Web
 Exec=firefox %u
-Icon=firefox
+Icon=/opt/firefox/browser/chrome/icons/default/default128.png
 Terminal=false
 Type=Application
 MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;text/mml;x-scheme-handler/http;x-scheme-handler/https;
@@ -101,7 +109,9 @@ EOF
 
 sudo chown "$(whoami)":"$(whoami)" /usr/share/applications/firefox-stable.desktop
 
+echo -e "\e[32;1mFirefox successfully installed!\e[m"
+
 # ---------------------------------------------------------------------------------------------------- #
 # ----------------------------------------------- CLEANUP -------------------------------------------- #
 ### Clean downloaded file.
-rm /home/teste/"$(id -nu 1000)"/firefox*.bz2
+rm /home/"$(id -nu 1000)"/firefox*.bz2
